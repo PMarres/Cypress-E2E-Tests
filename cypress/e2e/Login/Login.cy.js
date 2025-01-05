062b20695a62a21e35600d20d92afed87b993e5f
Pieter Marres <pietermarres@gmail.com>
2025-01-05 17:18:26 +0100

    Update Login.cy.js

--- cypress/e2e/Login/Login.cy.js
+++ cypress/e2e/Login/Login.cy.js
@@ -5,20 +5,12 @@ describe('Login', () => {
 
     it('Login Standard User', () => {
         cy.Login("standard_user", "secret_sauce")
-        // cy.get('input[name=user-name]').type("standard_user")
-        // cy.get('input[name=password]').type("secret_sauce")
-        // cy.get('input[id="login-button"]').click()
-
         cy.url().should('include', '/inventory.html')
-        //cy.getCookie('your-session-cookie').should('exist')
+        cy.getCookie('session-username').should('exist')
     })
 
     it('Login wrong', () => {
         cy.Login("standard_user", "secretsauce")
-        // cy.get('input[name=user-name]').type("standard_user")
-        // cy.get('input[name=password]').type("secretsauce")
-        // cy.get('input[id="login-button"]').click()
-
         cy.get('h3[data-test="error"]').should('exist').should('contain', 'Username and password do not match')
     })
 
